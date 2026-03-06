74d4cabc2e8c18c554c088ef4e2641f18faadfe2
hazoe-dev <hatran.dev@gmail.com>
2026-03-06 16:34:50 +0700

    chore: rename files and update libraries.

--- src/main/java/dev/hazoe/exercise/api/controller/ItemController.java
+++ src/main/java/dev/hazoe/exercise/api/controller/ItemController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ProductController {
+public class ItemController {
 
     private final String URL = "https://jsonmock.hackerrank.com/api/inventory";
     RestTemplate restTemplate = new RestTemplate();
