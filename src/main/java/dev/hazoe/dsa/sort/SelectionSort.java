5cf98bdfaee8c0e9016edf0ad2f9b9e9338badab
hazoe-dev <hatran.dev@gmail.com>
2026-03-08 22:57:29 +0700

    chore: notice stability for selection sort

--- src/main/java/dev/hazoe/dsa/sort/SelectionSort.java
+++ src/main/java/dev/hazoe/dsa/sort/SelectionSort.java
@@ -34,6 +34,31 @@ public class SelectionSort {
         nums[i] = temp;
     }
 
+    public int[] stableSelectionSort(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int minValue = nums[minIndex];
+
+            while (minIndex > i) {
+                nums[minIndex] = nums[minIndex - 1];
+                minIndex--;
+            }
+
+            nums[i] = minValue;
+        }
+
+        return nums;
+    }
+
     public static void main(String[] args) {
         int[] nums = {5, 4, 3, 2, 1};
         SelectionSort sort = new SelectionSort();
