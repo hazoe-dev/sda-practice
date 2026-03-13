15e204284a52ae33fff6277f1a8414bb05167b07
hazoe-dev <hatran.dev@gmail.com>
2026-03-13 13:49:12 +0700

    chore: practice TDD with parameterized test.

--- src/test/java/dev/hazoe/tdd/utils/PhoneNumberValidatorTest.java
+++ src/test/java/dev/hazoe/tdd/utils/PhoneNumberValidatorTest.java
@@ -1,48 +1,29 @@
 package dev.hazoe.tdd.utils;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class PhoneNumberValidatorTest {
+class PhoneNumberValidatorTest {
     private PhoneNumberValidator underTest;
     @BeforeEach
-    public void setUp(){
+    void setUp(){
         underTest = new PhoneNumberValidator();
     }
 
-    @Test
-    public void isShouldValidatePhoneNumber() {
-        //Given
-        String phoneNumber = "+44 5555-1234";
-        //When
-        boolean isValid = underTest.test(phoneNumber);
-        //Then
-        assertTrue(isValid);
-    }
+    @ParameterizedTest
+    @CsvSource({
+            "+44 5555-1234, true",
+            "44 5555-1234, false",
+            "+44 5555-123456, false"
+    })
+    void isShouldValidatePhoneNumber(String phoneNumber, boolean expectedResult) {
 
-    @Test
-    @DisplayName("Should fail when phone number does not start with +")
-    public void isShouldValidatePhoneNumberWhenIncorrectNumberWithoutStartingByPlusSign() {
-        //Given
-        String phoneNumber = "44 5555-1234";
         //When
         boolean isValid = underTest.test(phoneNumber);
         //Then
-        assertFalse(isValid);
-    }
-
-    @Test
-    @DisplayName("Should fail when length is bigger than 11")
-    public void isShouldValidatePhoneNumberWhenIncorrectNumberAndLengthBiggerThan11() {
-        //Given
-        String phoneNumber = "+44 5555-123456";
-        //When
-        boolean isValid = underTest.test(phoneNumber);
-        //Then
-        assertFalse(isValid);
+        assertThat(isValid).isEqualTo(expectedResult);
     }
 }
